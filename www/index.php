4e07cef949b508f7164efa2cce5cde3256c1ffea
Harry Merritt <harry@merritt.es>
2019-05-29 19:11:51 +0100

    set-up www/ folder structure

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <meta name='author' content='Harry Merritt' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes'>
+
+    <title>Internet Radio</title>
+
+
+    <link type='x-image/icon' rel='icon' href='favicon.ico' sizes='16x16'>
+    <link type='x-image/icon' rel='icon' href='img/logo/favicon-32.ico' sizes='32x32'>
+    <link type='x-image/icon' rel='icon' href='img/logo/favicon-48.ico' sizes='48x48'>
+    <link type='x-image/icon' rel='icon' href='img/logo/favicon-64.ico' sizes='64x64'>
+    <link type='x-image/icon' rel='icon' href='img/logo/favicon-128.ico' sizes='128x128'>
+    <link type='x-image/icon' rel='icon' href='img/logo/favicon-256.ico' sizes='256x256'>
+
+    <link rel='apple-touch-icon' href='img/logo/favicon-64.png'>
+    <link rel='apple-touch-icon' sizes='152x152' href='img/logo/favicon-152.png'>
+    <link rel='apple-touch-icon' sizes='180x180' href='img/logo/favicon-180.png'>
+    <link rel='apple-touch-icon' sizes='167x167' href='img/logo/favicon-167.png'>
+    <link rel='apple-touch-startup-image' href='favicon-180.png'>
+    <meta name='apple-mobile-web-app-title' content='Internet Radio'>
+
+
+    <link href='css/main.css' rel='stylesheet' type='text/css'>
+
+    <script src='js/libs/jquery.js' type='text/javascript'></script>
+    <script src='js/main.js' type='text/javascript'></script>
+
+</head>
+<body>
