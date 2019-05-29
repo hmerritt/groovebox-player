03d7fc808dead0be53391f4ca8f569e428246c0c
Harry Merritt <harry@merritt.es>
2019-05-29 19:57:21 +0100

    create index.php and link resources

--- www/index.php
+++ www/index.php
@@ -1,32 +1,48 @@
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 <head>
 
-    <meta name='author' content='Harry Merritt' />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes'>
+    <!--  metadata  -->
+    <meta name="author" content="https://github.com/Hmerritt" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes">
 
     <title>Internet Radio</title>
 
 
-    <link type='x-image/icon' rel='icon' href='favicon.ico' sizes='16x16'>
-    <link type='x-image/icon' rel='icon' href='img/logo/favicon-32.ico' sizes='32x32'>
-    <link type='x-image/icon' rel='icon' href='img/logo/favicon-48.ico' sizes='48x48'>
-    <link type='x-image/icon' rel='icon' href='img/logo/favicon-64.ico' sizes='64x64'>
-    <link type='x-image/icon' rel='icon' href='img/logo/favicon-128.ico' sizes='128x128'>
-    <link type='x-image/icon' rel='icon' href='img/logo/favicon-256.ico' sizes='256x256'>
+    <!--  tab icons  -->
+    <link type="x-image/icon" rel="icon" href="favicon.ico" sizes="16x16">
+    <link type="x-image/icon" rel="icon" href="img/logo/favicon-32.ico" sizes="32x32">
+    <link type="x-image/icon" rel="icon" href="img/logo/favicon-48.ico" sizes="48x48">
+    <link type="x-image/icon" rel="icon" href="img/logo/favicon-64.ico" sizes="64x64">
+    <link type="x-image/icon" rel="icon" href="img/logo/favicon-128.ico" sizes="128x128">
+    <link type="x-image/icon" rel="icon" href="img/logo/favicon-256.ico" sizes="256x256">
 
-    <link rel='apple-touch-icon' href='img/logo/favicon-64.png'>
-    <link rel='apple-touch-icon' sizes='152x152' href='img/logo/favicon-152.png'>
-    <link rel='apple-touch-icon' sizes='180x180' href='img/logo/favicon-180.png'>
-    <link rel='apple-touch-icon' sizes='167x167' href='img/logo/favicon-167.png'>
-    <link rel='apple-touch-startup-image' href='favicon-180.png'>
-    <meta name='apple-mobile-web-app-title' content='Internet Radio'>
+    <!--  apple icons  -->
+    <link rel="apple-touch-icon" href="img/logo/logo-64.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/logo/logo-152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/logo/logo-180.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="img/logo/logo-167.png">
+    <link rel="apple-touch-startup-image" href="img/logo/logo-180.png">
+    <meta name="apple-mobile-web-app-title" content="Internet Radio">
 
 
-    <link href='css/main.css' rel='stylesheet' type='text/css'>
+    <!--  styles  -->
+    <link href="css/main.css" rel="stylesheet" type="text/css">
 
-    <script src='js/libs/jquery.js' type='text/javascript'></script>
-    <script src='js/main.js' type='text/javascript'></script>
+    <!--  scripts  -->
+    <script src="js/libs/jquery.js" type="text/javascript"></script>
+    <script src="js/main.js" type="text/javascript"></script>
 
 </head>
 <body>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
